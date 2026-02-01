67207b7cd968fdc8bec0c5b75a7117d35e8b2870
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-02-01 07:20:37 -0800

    Add localization support, routing, and styling enhancements
    - Added Arabic and English translations for new terms in ar.json and en.json.
    - Expanded AppRoutes with new screens for search results and article details.
    - Implemented RouterGenerationConfig for managing app routes.
    - Updated main.dart to use ScreenUtil and refactored MyApp to NewsApp.
    - Introduced AppTextStyles for consistent text styling using Google Fonts.
    - Added google_fonts dependency in pubspec.yaml.
    - Updated widget tests to reflect changes in the main app structure.
    - Removed unused app_assets.dart file.

---
+++ lib/core/routing/router_generator.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:news_app/core/routing/app_routs.dart';
+
+
+class RouterGenerationConfig {
+  static GoRouter goRouter =
+      GoRouter(initialLocation: AppRoutes.homeScreen, routes: [
+    GoRoute(
+        path: AppRoutes.homeScreen,
+        name: AppRoutes.homeScreen,
+        builder: (context, state) => HomeScreen()),
+    GoRoute(
+        path: AppRoutes.searchScreen,
+        name: AppRoutes.searchScreen,
+        builder: (context, state) => Container()),
+    GoRoute(
+        path: AppRoutes.searchResultScreen,
+        name: AppRoutes.searchResultScreen,
+        builder: (context, state) => Container()),
+    GoRoute(
+        path: AppRoutes.articleDetailsScreen,
+        name: AppRoutes.articleDetailsScreen,
+        builder: (context, state) => Container()),
+  ]);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,44 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/core/routing/router_generator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+
+  DioHelper.initDio();
+
   runApp(
     EasyLocalization(
-      supportedLocales: [Locale('en'), Locale('ar')],
-      path: 'assets/translation',
-      fallbackLocale: Locale('ar'),
-      child: MyApp(),
+      supportedLocales: const [Locale('en'), Locale("ar")],
+      path:
+          'assets/translation', 
+      fallbackLocale: const Locale('ar'),
+      // startLocale: const Locale('ar'),
+      child: const NewsApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void _changeLanguage() {
-    setState(() {
-      if (context.locale.languageCode == 'en') {
-        context.setLocale(Locale('ar'));
-      } else {
-        context.setLocale(Locale('en'));
-      }
-    });
-  }
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("current_language  is: ${context.locale.languageCode}"),
-            Text(
-              "name".tr(),
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _changeLanguage,
-        tooltip: 'change language',
-        child: const Icon(Icons.change_circle),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: false,
+      builder: (context, child) {
+        return MaterialApp.router(
+          title: 'News App',
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          routerConfig: RouterGenerationConfig.goRouter,
+        );
+      },
     );
   }
 }
