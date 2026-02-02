42ca6e2d9dc10e30a2d43ac04a3c5159d5cf6498
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-02-02 11:27:40 -0800

    Add CustomCategoryItemWidget and TopHeadlineWidget for improved UI components

---
+++ lib/features/home/widgets/custom_category_item_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/core/style/app_text_style.dart';
+
+class CustomCategoryItemWidget extends StatelessWidget {
+  const CustomCategoryItemWidget({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 12.w),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(56.r),
+        splashColor: Color(0xffE9EEFA),
+        onTap: () {},
+        child: Container(
+          height: 32.h,
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(
+            horizontal: 24.w,
+            vertical: 6.h,
+          ),
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 3.w,
+              color: Color(0xffE9EEFA),
+            ),
+            borderRadius: BorderRadius.circular(56.r),
+            color: Colors.transparent,
+          ),
+          child: Text(
+            title,
+            style: AppTextStyles.black14SemiBold,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/widgets/top_headline_widget.dart
@@ -0,0 +1,62 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/core/style/app_text_style.dart';
+import 'package:news_app/core/widgets/spacing.dart';
+
+class TopHeadlineWidget extends StatelessWidget {
+  const TopHeadlineWidget({
+    super.key,
+    required this.title,
+    required this.author,
+    required this.imageUrl,
+    required this.date,
+  });
+  final String title;
+  final String author;
+  final String imageUrl;
+  final String date;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8.r),
+            child: imageUrl == ""
+                ? Container(
+                    color: Colors.grey.shade300,
+                    height: 206.h,
+                    width: 320.w,
+                  )
+                : CachedNetworkImage(
+                    imageUrl: imageUrl,
+                    height: 206.h,
+                    fit: BoxFit.fill,
+                    placeholder: (context, url) => Container(
+                      color: Colors.grey.shade300,
+                      height: 206.h,
+                      width: 320.w,
+                    ),
+                    errorWidget: (context, url, error) => Container(
+                      color: Colors.grey.shade300,
+                      height: 206.h,
+                      width: 320.w,
+                      child: Icon(Icons.error),
+                    ),
+                  ),
+          ),
+        ),
+        HeightSpace(16),
+        Text(
+          title,
+          style: AppTextStyles.titlesStyles.copyWith(fontSize: 18.sp),
+        ),
+        HeightSpace(12),
+        Text('By $author • $date', style: AppTextStyles.grey14Regular),
+      ],
+    );
+  }
+}
