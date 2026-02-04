958c0ea5b702826f2025532f35e03df50591e008
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-02-03 17:45:17 -0800

    Add SearchResultService for handling search queries and responses

---
+++ lib/features/search_result_screen/services/search_result_services.dart
@@ -0,0 +1,30 @@
+import 'package:news_app/core/constant/app_constant.dart';
+import 'package:news_app/core/networking/api_endpont.dart';
+import 'package:news_app/core/networking/dio_helper.dart';
+import 'package:news_app/features/home/models/top_head_line_model.dart';
+class SearchResultService {
+  Future<ArticlesMoodel> searchItemByName(String query) async {
+    if (query.trim().isEmpty) {
+      throw Exception('EMPTY_QUERY');
+    }
+
+    try {
+      final response = await DioHelper.getRequest(
+        endpoint: ApiEndpont.searchEndpoint,
+        query: {
+          "apiKey": AppConstant.newsApiKey,
+          "q": query,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return ArticlesMoodel.fromJson(response.data);
+      } else {
+        throw Exception('SERVER_ERROR');
+      }
+    } catch (e) {
+      throw Exception('NETWORK_ERROR');
+    }
+  }
+}
+
