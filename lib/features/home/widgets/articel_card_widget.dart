37e0685eee8b51a0b204269c101b881e7596c00f
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-02-03 18:44:39 -0800

    Refactor ArticelCardWidget to use Article model and improve data handling

--- lib/features/home/widgets/articel_card_widget.dart
+++ lib/features/home/widgets/articel_card_widget.dart
@@ -1,68 +1,72 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:news_app/core/routing/app_routs.dart';
 import 'package:news_app/core/widgets/spacing.dart';
+import 'package:news_app/features/home/models/top_head_line_model.dart';
 
 class ArticelCardWidget extends StatelessWidget {
   const ArticelCardWidget({
     super.key,
-    required this.title,
-    required this.author,
-    required this.imageUrl,
-    required this.date,
+    required this.article,
   });
-  final String title;
-  final String author;
-  final String imageUrl;
-  final String date;
+  final Article article;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: () {
+        GoRouter.of(
+          context,
+        ).pushNamed(AppRoutes.articleDetailsScreen, extra: article);
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  article.title ?? 'No Title',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              HeightSpace(8),
-              Text(
-                'By $author · $date',
-                style: TextStyle(
-                  fontSize: 14.sp,
-                  color: Colors.grey,
+                HeightSpace(8),
+                Text(
+                  'By ${article.author ?? 'Unknown Author'} · ${article.publishedAt ?? 'Unknown Date'}',
+                  style: TextStyle(
+                    fontSize: 14.sp,
+                    color: Colors.grey,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
 
-        ClipRRect(
-          borderRadius: BorderRadius.circular(8.r),
-          child: CachedNetworkImage(
-            imageUrl: imageUrl,
-            width: 112.w,
-            height: 80.h,
-            fit: BoxFit.cover,
-            placeholder: (context, url) => Container(
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.r),
+            child: CachedNetworkImage(
+              imageUrl: article.urlToImage ?? '',
               width: 112.w,
               height: 80.h,
-              alignment: Alignment.center,
-              child: CircularProgressIndicator(strokeWidth: 2),
-            ),
-            errorWidget: (context, url, error) => Image.network(
-              'https://ichef.bbci.co.uk/images/ic/624x351/p0gdcnjt.jpg',
               fit: BoxFit.cover,
+              placeholder: (context, url) => Container(
+                width: 112.w,
+                height: 80.h,
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(strokeWidth: 2),
+              ),
+              errorWidget: (context, url, error) => Image.network(
+                'https://ichef.bbci.co.uk/images/ic/624x351/p0gdcnjt.jpg',
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
